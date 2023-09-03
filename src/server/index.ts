9d14a80d57f244ab79f4107bf8680ce935f017bc
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-03 19:05:30 +0200

    Test pingController

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import pingController from "./controllers/pingController/pingController.js";
 
 const app = express();
 app.disable("x-powered-by");
+
+app.get("/", pingController);
 
 export default app;
