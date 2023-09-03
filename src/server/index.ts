986548e5b02e8ad782bd2b56c6fe897fa1c0394b
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-03 19:14:39 +0200

    Add '/' path with pingController

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import pingController from "./controllers/pingController/pingController.js";
 
 const app = express();
 app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", pingController);
 
