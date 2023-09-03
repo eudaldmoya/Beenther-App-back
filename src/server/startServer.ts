4f73679ed4f9aa74fadfefeb3ba4d3dedb56a842
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-03 17:30:05 +0200

    Add startServer function

---
+++ src/server/startServer.ts
@@ -0,0 +1,12 @@
+import debugCreator from "debug";
+import app from "./index.js";
+
+const debug = debugCreator("destinations:server:start");
+
+const startServer = (port: number) => {
+  app.listen(port, () => {
+    debug(`Listening on http://localhost:${port}`);
+  });
+};
+
+export default startServer;
