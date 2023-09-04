7cf326663af54f31c88e2bdd661b846e78857562
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-04 10:03:50 +0200

    Create connectToDatabase function

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import generalErrorHandler from "./controllers/generalErrorHandler/generalErrorHandler.js";
 import pingController from "./controllers/pingController/pingController.js";
+import endpointNotFound from "./middlewares/endpointNotFound/endpointNotFound.js";
 
 const corsConfig = {
   origin: [process.env.ALLOW_ORIGIN_PROD!, process.env.ALLOW_ORIGIN_LOCAL!],
@@ -19,5 +21,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", pingController);
+
+app.use(endpointNotFound);
+app.use(generalErrorHandler);
 
 export default app;
