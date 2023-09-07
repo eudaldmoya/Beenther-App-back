256f5d2a6665d907edad8eb1727fda573589449f
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-07 15:22:53 +0200

    Fix paths export and type paths

---
+++ src/server/paths/types.ts
@@ -0,0 +1,4 @@
+export interface Paths {
+  root: string;
+  destinations: string;
+}
--- src/server/routers/destinationsRouter.ts
+++ src/server/routers/destinationsRouter.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { getDestinations } from "../controllers/destinations/destinationsControllers.js";
-import { root } from "../utils/paths.js";
+import { paths } from "../paths/paths.js";
 
 const destinationsRouter = express.Router();
 
-destinationsRouter.get(root, getDestinations);
+destinationsRouter.get(paths.root, getDestinations);
 
 export default destinationsRouter;
