e7569d6f137cbac57b8c20caf756044f974fceea
eudaldmoya <eudald.moya.8@gmail.com>
2023-09-13 09:32:15 +0200

    Fix tests to work with findByIdAndDelete

--- src/server/controllers/destinations/__tests__/deleteDestination.test.ts
+++ src/server/controllers/destinations/__tests__/deleteDestination.test.ts
@@ -1,8 +1,7 @@
+import { type NextFunction, type Request, type Response } from "express";
 import Destination from "../../../../database/models/Destination";
 import { mongooseIdMockD1 } from "../../../../mocks/destinationsMock";
-import { type Request, type Response, type NextFunction } from "express";
 import { deleteDestination } from "../destinationsControllers";
-import CustomError from "../../../CustomError/CustomError";
 
 describe("Given a getDestinations controller", () => {
   const req: Partial<Request> = {
@@ -20,8 +19,8 @@ describe("Given a getDestinations controller", () => {
     test("Then it should respond with status 200", async () => {
       const expectedStatusCode = 200;
 
-      Destination.deleteOne = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue({ acknowledged: true }),
+      Destination.findByIdAndDelete = jest.fn().mockReturnValue({
+        exec: jest.fn(),
       });
 
       await deleteDestination(req as Request, res as Response, next);
@@ -31,11 +30,11 @@ describe("Given a getDestinations controller", () => {
   });
 
   describe("when it receives a request with a destinationId: mongooseIdMockD1, a response and a next function", () => {
-    test("Then the response json method should be called with { acknowledged: true}", async () => {
+    test("Then the response json method should be called with 'Destination deleted successfully'", async () => {
       const expectedMessage = "Destination deleted successfully";
 
-      Destination.deleteOne = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue({ acknowledged: true }),
+      Destination.findByIdAndDelete = jest.fn().mockReturnValue({
+        exec: jest.fn(),
       });
 
       await deleteDestination(req as Request, res as Response, next);
@@ -44,29 +43,11 @@ describe("Given a getDestinations controller", () => {
     });
   });
 
-  describe("when it receives a request with a non existant destinationId, a response and a next function", () => {
-    test("Then the next function should be called with 'Destination not found'", async () => {
-      const customError = new CustomError(
-        "Destination not found",
-        404,
-        "Destination not found",
-      );
-
-      Destination.deleteOne = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue({ acknowledged: false }),
-      });
-
-      await deleteDestination(req as Request, res as Response, next);
-
-      expect(next).toHaveBeenCalledWith(customError);
-    });
-  });
-
   describe("when it receives a request without a destinationId, a response and a next function", () => {
     test("Then the next function should be called with 'Could not delete the destination'", async () => {
       const error = new Error();
 
-      Destination.deleteOne = jest.fn().mockReturnValue({
+      Destination.findByIdAndDelete = jest.fn().mockReturnValue({
         exec: jest.fn().mockRejectedValue({}),
       });
 
